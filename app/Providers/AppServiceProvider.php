fc9454e9c54ded60d4c8a56b186e7bdf7645dcf5
davy39 <davy@ik.me>
2025-11-26 19:35:04 +0100

    seed db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,39 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
-        Model::unguard();
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        // --- FIX URL STACKBLITZ ---
-        $url = config('app.url');
-        
-        // Si l'URL est définie et qu'elle contient 'webcontainer' (StackBlitz)
-        if (!empty($url) && str_contains($url, 'webcontainer.io')) {
-            URL::forceRootUrl($url);
-            URL::forceScheme('https');
-        }
-        // --------------------------
-        
-        $this->bootRoute();
-    }
-    
-    public function bootRoute(): void
-    {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
+        //
     }
 }
